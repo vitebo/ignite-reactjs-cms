f5fbd2a2b736bcb83beb2a5bda80ef1b4634834b
vitebo <vitebo@hotmail.com>
2021-04-18 23:57:29 -0300

    feat(component): create header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,10 @@
-export default function Header() {
-  // TODO
+import Image from 'next/image';
+import styles from './header.module.scss';
+
+export function Header(): JSX.Element {
+  return (
+    <header className={styles.Container}>
+      <Image src="/images/logo.svg" alt="logo" width="auto" height="auto" />
+    </header>
+  );
 }
